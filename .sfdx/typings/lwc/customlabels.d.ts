86ebf2ce93064004e356a536223925574fae6a78
Ritika <gumber.ritika@gmail.com>
2024-04-30 11:44:28 +0530

    feature1

---
+++ .sfdx/typings/lwc/customlabels.d.ts
@@ -0,0 +1,8 @@
+declare module "@salesforce/label/c.CMM_MDM_Intake_Queue_Name" {
+    var CMM_MDM_Intake_Queue_Name: string;
+    export default CMM_MDM_Intake_Queue_Name;
+}
+declare module "@salesforce/label/c.CMM_Sales_Ops_Intake_Queue_Name" {
+    var CMM_Sales_Ops_Intake_Queue_Name: string;
+    export default CMM_Sales_Ops_Intake_Queue_Name;
+}
